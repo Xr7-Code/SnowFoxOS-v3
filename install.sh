3449d2ecd3b286a3a444fbb79e45f9338e412429
Xr7-Code <alexander-ludwig@tutamail.com>
2026-04-09 16:26:55 +0200

    Comment out version and description lines in install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-SnowFoxOS v3.2 - Void Fixed (v2 Feature Complete + i3)
+#SnowFoxOS v3.2 - Void Fixed (v2 Feature Complete + i3)
 
-Stable base + restored v2 desktop environment
+#Stable base + restored v2 desktop environment
 
 RED='\033[0;31m' GREEN='\033[0;32m' CYAN='\033[0;36m' YELLOW='\033[1;33m' BOLD='\033[1m' NC='\033[0m'
 
